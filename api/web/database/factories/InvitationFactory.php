89791118f19fe5859a0c35ffc8980cbe159f94bf
HinanoOno <hinagon1231@icloud.com>
2024-02-11 11:54:51 +0900

    feat: ダミーデータを作成

---
+++ api/web/database/factories/InvitationFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class InvitationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'inviter_id' => User::factory(), 
+            'invited_id' => User::factory(),
+            'date' => now(),
+        ];
+    }
+}
